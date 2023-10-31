22de0c52f44593d2c917b94488fb66f5a0a591ce
xanderkoen <9006097@student.zadkine.nl>
2023-10-31 14:06:32 +0100

    HTML structuur af
    kat foto is placeholder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'clouds1' : "url('/Images/clouds1.png')",
+        'clouds2' : "url('/Images/clouds2.png')",
+        'clouds3' : "url('/Images/clouds3.png')",
+        'clouds4' : "url('/Images/clouds4.png')"
+
+      },
+      colors: {
+        'donkergrijs': '#47525E',
+      }
+    },
   },
   plugins: [],
 }
