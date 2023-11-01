008bb906a0fc72d2957ea5a0060fd63cf8169fc6
Zino Schubert <1010115@hr.nl>
2023-11-01 13:31:54 +0100

    Functie gemaakt die image download

--- png_maker/js/png_maker.js
+++ png_maker/js/png_maker.js
@@ -7,5 +7,18 @@ ctx.fillRect(10, 10, 50, 50);
 ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
 ctx.fillRect(30, 30, 50, 50);
 
-let img = canvas.toDataURL("image/png")
-document.write('<img src="'+img+'"/>');
+
+
+let download = document.createElement('a')
+
+download.download = "vedute.png"
+download.innerText = "DOWNLOAD VEDUTE"
+document.body.appendChild(download)
+
+function downloadImage (canvas){
+    let img = canvas.toDataURL("image/png")
+    download.href = img
+}
+
+// TEST
+downloadImage(canvas)
