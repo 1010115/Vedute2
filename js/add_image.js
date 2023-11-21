252f12a91b5aaa6cb46bcd41a0105b03b9b24705
vultouri03 <youridegier@gmail.com>
2023-11-21 13:56:50 +0100

    adapted images to p5
    there are now 2 layers for images and the image layer only apears when you add in an image

--- js/add_image.js
+++ js/add_image.js
@@ -1,112 +1,132 @@
 window.addEventListener('DOMContentLoaded', init);
-let currentX = 0;
-let currentY = 0;
-let lastX = 0;
-let lastY = 0;
-let canvas, context;
-let imageCanvas, imagecontext;
+
 let img;
-let mouseX;
-let mouseY;
-let isDraggable = false;
+let staticImg;
+let imgDiv;
+let confirmImg;
+let imgCorrect = false;
+let dragging = false; // Is the object being dragged?
+let rollover = false; // Is the mouse over the ellipse?
+
+let x, y, w, h; // Location and size
+let staticX, staticY;
+let offsetX, offsetY; // Mouseclick offset
+let imageLayer;
 
 function init() {
   //gets the necesarry html elements
-    const input = document.getElementById("files");
-    const output = document.getElementById("output");
-    canvas = document.getElementById("canvas");
-    context = canvas.getContext("2d");
-    //checks if the user has uploaded any files and then initializes the image
-    if (files.length === 0) {
-      output.textContent = "No files selected.";
-      return;
-     } else {
-    input.addEventListener("change", function(e) {
-        if(e.target.files) {
-        const imageFile = input.files[0];
-        reader = new FileReader();
-        reader.readAsDataURL(imageFile);
-          reader.onloadend = function(e) {
-            img = new Image(100);
-            img.src = e.target.result;
-            currentX = 0;
-            currentY = 0;
-          img.onload = function(ev) {
-            createImageCanvas();
-            _Go();
-            
-          };
-         }
-            }}
-      )}
+  confirmImg = document.getElementById('image-confirm');
+  confirmImg.addEventListener('click', confirmClickHandler);
+  imgDiv = document.getElementById('imageCanvas');
+  imgDiv.classList.add('hidden');
 }
 
-function _Go() {
-  _MouseEvents();
-  
-  setInterval(function() {
-    _ResetCanvas();
-    _DrawImage();
-  }, 1000/30);
-}
-
-function _ResetCanvas() {
-  if(isDraggable) {
-    imagecontext.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
+let s1 = function (sketch) {
+  sketch.setup = function () {
+    let canvas1 = sketch.createCanvas(406, 560);
+    canvas1.parent('canvasCanvas');
+    input = sketch.createFileInput(sketch.handleFile);
+    input.parent('image-button')
   }
-  
-}
-
-function _MouseEvents() {
-  
-  imageCanvas.onmousedown = function(e) {
-    
-    mouseX = e.pageX - this.offsetLeft;
-    mouseY = e.pageY - this.offsetTop;
-    console.log(mouseX, mouseY)
-    
-
-    if (mouseX >= (currentX - 400/2 ) &&
-        mouseX <= (currentX + 400/2) &&
-        mouseY >= (currentY -600/2) &&
-        mouseY <= (currentY + 600/2)) {
-      isDraggable = true;
-      console.log(isDraggable);
+  sketch.draw = function () {
+    //for canvas 1
+    sketch.background(255, 0);
+    if (img && imgCorrect) {
+      console.log(img);
+      sketch.image(staticImg, staticX, staticY, w, h);
     }
-  };
-  imageCanvas.onmousemove = function(e) {
-    if (isDraggable) {
-      currentX = e.pageX - this.offsetLeft;
-      currentY = e.pageY - this.offsetTop;
-    }
-  };
-  imageCanvas.onmouseup = function(e) {
-    isDraggable = false;
-    console.log(currentX, currentY);
-    _DrawImage();
-  };
-  imageCanvas.onmouseout = function(e) {
-    isDraggable = false;
-  };
-}
-function _DrawImage() {
 
-    imagecontext.drawImage(img, currentX * 2, currentY * 2, 400, 600);
-    lastX = currentX * 2;
-    lastY = currentY * 2;
-}
-
-function createImageCanvas() {
-  let list = canvas.classList;
-  let container = document.getElementById("canvasContainer");
-  imageCanvas = document.createElement("canvas");
-  imageCanvas.width = canvas.width;
-  imageCanvas.height = canvas.height;
-  imagecontext = imageCanvas.getContext("2d");
-  for(let i =0; i <= list.length; i++) {
-    imageCanvas.classList.add(list[i]);
   }
-  imageCanvas.classList.add(["z-10", "absolute"])
-  console.log(container);
-  container.append(imageCanvas);
+  sketch.handleFile = function (file) {
+
+    if (file.type === 'image') {
+      img = sketch.createImg(file.data, '');
+      img.hide();
+      if (imgDiv.classList.contains('hidden')) {
+        imgDiv.classList.toggle('hidden');
+      }
+    } else {
+      img = null;
+    }
+  }
+
+
 }
+
+mainCanvas = new p5(s1);
+
+let s2 = function (sketch) {
+  sketch.setup = function () {
+    let canvas2 = sketch.createCanvas(406, 560);
+    canvas2.parent('imageCanvas');
+    // Starting location
+    x = 0;
+    y = 0;
+
+    // Dimensions
+    w = 100;
+    h = 100;
+  }
+  sketch.draw = function () {
+    //for canvas 2
+
+    sketch.clear();
+
+    // Is mouse over object
+    if (sketch.mouseX > x && sketch.mouseX < x + w && sketch.mouseY > y && sketch.mouseY < y + h) {
+      rollover = true;
+    } else {
+      rollover = false;
+    }
+
+    // Adjust location if being dragged
+    if (dragging) {
+      x = sketch.mouseX + offsetX;
+      y = sketch.mouseY + offsetY;
+    }
+
+    sketch.stroke(0);
+    if (img) {
+
+      sketch.image(img, x, y, w, h);
+    }
+
+  }
+
+  sketch.mousePressed = function () {
+    if (sketch.mouseX > x && sketch.mouseX < x + w && sketch.mouseY > y && sketch.mouseY < y + h) {
+      dragging = true;
+
+      offsetX = x - sketch.mouseX;
+      offsetY = y - sketch.mouseY;
+    }
+
+
+  }
+  sketch.mouseReleased = function () {
+    // Quit dragging
+    dragging = false;
+  }
+}
+
+imageLayer = new p5(s2);
+
+function confirmClickHandler() {
+  imgCorrect = true;
+  staticImg = img;
+  staticX = x;
+  staticY = y;
+  if (!imgDiv.classList.contains('hidden') && imgDiv !== undefined) {
+    imgDiv.classList.add('hidden');
+  }
+}
+
+
+
+function keyTyped() {
+  if (key === 'a') {
+    save('image.png');
+  }
+}
+
+
