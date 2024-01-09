27e407ccc03e99194a1be9d1c7b1d61123feb5c9
Zino Schubert <1010115@hr.nl>
2024-01-09 11:22:03 +0100

    Tutorial styling en uitbreiding

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,23 +1,33 @@
-let tutorialField = document.getElementById('tutorialField')
-let tutorialBtn = document.getElementById('tutorialBtn')
+let tutorialField = document.getElementById('tutorial-field')
+let tutorialBtn = document.getElementById('tutorial-button')
 let screen = document.getElementById('screen')
 let pageNmbr = 0
 let popUpState = false
+let tutorialLength = 5
+
+console.log(tutorialBtn)
 
 let tutorialImg = document.createElement('img')
 let nextBtn = document.createElement('button')
 let prevBtn = document.createElement('button')
+let exitBtn = document.createElement('button')
 
 tutorialImg.src = `../assets/tutorial_${pageNmbr}.png`
-nextBtn.innerText = '>'
-prevBtn.innerText = '<'
+nextBtn.innerText = '->'
+prevBtn.innerText = '<-'
+exitBtn.innerText = 'X'
 
-nextBtn.classList = ' fixed right-1000'
-prevBtn.classList = ' fixed left-100 top-300'
-// tutorialImg.tagName = "h-50% fit"
+nextBtn.classList = ' m-1 rounded-full bg-slate-50 hover:border-2 border-black w-1/6'
+prevBtn.classList = ' m-1 rounded-full bg-slate-50 hover:border-2 border-black w-1/6'
+exitBtn.classList = ' m-1 rounded-full bg-slate-50 hover:border-2 border-black w-1/6'
+tutorialImg.classList = ''
 
 
 tutorialBtn.addEventListener('click', () => {
+    popUp()
+})
+
+exitBtn.addEventListener('click', ()=> {
     popUp()
 })
 
@@ -27,13 +37,13 @@ function popUp() {
         tutorialField.appendChild(tutorialImg)
         tutorialField.appendChild(prevBtn)
         tutorialField.appendChild(nextBtn)
+        tutorialField.appendChild(exitBtn)
+        tutorialField.classList = 'h-full w-full flex flex-wrap gap-x-20 justify-center overflow-hidden'
         screen.classList = 'display: hidden overflow-y: hidden'
 
         popUpState = true
     } else {
-        tutorialField.removeChild(tutorialImg)
-        tutorialField.removeChild(prevBtn)
-        tutorialField.removeChild(nextBtn)
+        tutorialField.classList = 'display: hidden'
         screen.classList = 'grid grid-cols-10 place-content-center min-w-screen min-h-screen h-full bg-clouds2 bg-cover'
 
         popUpState = false
@@ -44,8 +54,8 @@ function popUp() {
         pageNmbr = pageNmbr + number;
         if (pageNmbr < 0) {
             pageNmbr = 0
-        } else if (pageNmbr > 3) {
-            pageNmbr = 3
+        } else if (pageNmbr > tutorialLength) {
+            pageNmbr = tutorialLength
         }
         tutorialImg.src = `../assets/tutorial_${pageNmbr}.png`
     }
