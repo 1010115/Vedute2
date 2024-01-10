ee76334166ffb8ce8b34729f1cd234a77e633ed8
Xander <xkoen@hotmail.com>
2024-01-10 18:15:29 +0100

    tutorial tweaks

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,35 +1,57 @@
-let tutorialField = document.getElementById('tutorial-field')
-let tutorialBtn = document.getElementById('tutorial-button')
-let screen = document.getElementById('screen')
+document.addEventListener("DOMContentLoaded", init)
+
+let tutorialField
+let tutorialBtn
+let screen
 let pageNmbr = 0
 let popUpState = false
 let tutorialLength = 6
 
-console.log(tutorialBtn)
+let tutorialImg
+let nextBtn
+let prevBtn
+let exitBtn
 
-let tutorialImg = document.createElement('img')
-let nextBtn = document.createElement('button')
-let prevBtn = document.createElement('button')
-let exitBtn = document.createElement('button')
+function init () {
+    exitBtn = document.createElement('button')
+    prevBtn = document.createElement('button')
+    nextBtn = document.createElement('button')
+    tutorialImg = document.createElement('img')
+    screen = document.getElementById('screen')
+    tutorialBtn = document.getElementById('tutorial-button')
+    tutorialField = document.getElementById('tutorial-field')
 
-tutorialImg.src = `../Images/tutorial_${pageNmbr}.png`
-nextBtn.innerHTML = `<img src="../assets/arrow-right.svg" alt="->"/>`
-prevBtn.innerHTML = `<img src="../assets/arrow-left.svg" alt="<-"/>`
-exitBtn.innerHTML = `<img src="../assets/xmark-solid.svg" alt="X"/>`
+    if (!localStorage.getItem("firsttime")) {
+        localStorage.setItem("firsttime", "true")
+    }
 
-nextBtn.classList = 'absolute left-3/4 bottom-2 rounded-full bg-slate-50 border border-2 hover:border-black w-1/6 flex justify-center'
-prevBtn.classList = 'absolute left-1/4 bottom-2 rounded-full bg-slate-50 border border-2 hover:border-black w-1/6 flex justify-center'
-exitBtn.classList = 'absolute left-2/4 bottom-2 rounded-full bg-slate-50 border border-2 hover:border-black w-1/6 flex justify-center'
-tutorialImg.classList = 'w-screen h-screen'
+    if (localStorage.getItem("firsttime") === "true") {
+        popUp()
+        localStorage.setItem("firsttime", "false")
+    }
 
+    tutorialImg.src = `../Images/tutorial_${pageNmbr}.png`
+    nextBtn.innerHTML = `<img src="../assets/arrow-right.svg" alt="->"/>`
+    prevBtn.innerHTML = `<img src="../assets/arrow-left.svg" alt="<-"/>`
+    exitBtn.innerHTML = `<img src="../assets/xmark-solid.svg" alt="X"/>`
 
-tutorialBtn.addEventListener('click', () => {
-    popUp()
-})
+    nextBtn.classList = 'absolute left-3/4 bottom-2 rounded-full bg-slate-50 border border-2 hover:border-black w-1/6 flex justify-center'
+    prevBtn.classList = 'absolute left-1/4 bottom-2 rounded-full bg-slate-50 border border-2 hover:border-black w-1/6 flex justify-center'
+    exitBtn.classList = 'absolute left-2/4 bottom-2 rounded-full bg-slate-50 border border-2 hover:border-black w-1/6 flex justify-center'
+    tutorialImg.classList = 'w-screen h-screen'
 
-exitBtn.addEventListener('click', ()=> {
-    popUp()
-})
+    tutorialBtn.addEventListener('click', popUp)
+
+    exitBtn.addEventListener('click', popUp)
+
+    nextBtn.addEventListener('click', () => {
+        pageTracker(1)
+    })
+
+    prevBtn.addEventListener('click', () => {
+        pageTracker(-1)
+    })
+}
 
 
 function popUp() {
@@ -50,20 +72,12 @@ function popUp() {
     }
 }
 
-    function pageTracker(number) {
-        pageNmbr = pageNmbr + number;
-        if (pageNmbr < 0) {
-            pageNmbr = 0
-        } else if (pageNmbr > tutorialLength) {
-            pageNmbr = tutorialLength
-        }
-        tutorialImg.src = `../Images/tutorial_${pageNmbr}.png`
+function pageTracker(number) {
+    pageNmbr = pageNmbr + number;
+    if (pageNmbr < 0) {
+        pageNmbr = 0
+    } else if (pageNmbr > tutorialLength) {
+        pageNmbr = tutorialLength
     }
-
-    nextBtn.addEventListener('click', () => {
-        pageTracker(1)
-    })
-
-    prevBtn.addEventListener('click', () => {
-        pageTracker(-1)
-    })
+    tutorialImg.src = `../Images/tutorial_${pageNmbr}.png`
+}
